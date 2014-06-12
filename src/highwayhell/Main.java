6ff5f891339d068775ec9e4e0fc92af141e43e63
JoseRivas <jose.rivas.1997@gmail.com>
2014-06-11 20:42:59 -0400

    Update Main.java

--- src/highwayhell/Main.java
+++ src/highwayhell/Main.java
@@ -1,18 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+//Main File draws the frame
 package highwayhell;
-
-/**
- *
- * @author Scotty
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         FraMain fraMain = new FraMain();
